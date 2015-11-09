e10e75b90361e3c4f3880ce7c3b9676ee9a68300
unknown <pascal.nguyen@u-psud.fr>
2015-11-09 10:51:45 +0100

    Ajout du TP1 KEBAILI Rémi

---
+++ workspace/CPOB - KEBAILI/Workspace/iuttp/CPOB_TP1/src/iut/gui/listeners/ApplicationErrorMessageDialog.java
@@ -0,0 +1,21 @@
+package iut.gui.listeners;
+
+import javax.swing.JOptionPane;
+
+public class ApplicationErrorMessageDialog extends
+		AbstractApplicationMessageDialog {
+
+	@Override
+	protected void showMessage(String level, String message) {
+		/** UTILISER l'AIDE FORUNIR DANS L'ENNONCE */
+		JOptionPane jop1 = new JOptionPane();
+		jop1.showMessageDialog(null, level, message, JOptionPane.ERROR_MESSAGE);
+	}
+
+	@Override
+	protected void showMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
