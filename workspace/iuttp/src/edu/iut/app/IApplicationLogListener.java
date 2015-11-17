a300f6ce352f385dc910313845061d847b8cfeb1
Clockwork9 <pascal.nguyen@u-psud.fr>
2015-11-17 08:40:22 +0100

    Ajout du TP2

--- workspace/iuttp/src/edu/iut/app/IApplicationLogListener.java
+++
@@ -1,9 +0,0 @@
-package edu.iut.app;
-
-public interface IApplicationLogListener {
-	/** TP1 : Créer la fuonction newMessage 
-	 * @param message 
-	 * @return */
-	
-	public abstract void newMessage(String message);
-}
